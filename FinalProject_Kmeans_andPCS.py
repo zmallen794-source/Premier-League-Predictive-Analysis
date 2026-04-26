be55e16499793c79ceea6703ed124d5a93180370
Zach Allen <zmallen794@gmail.com>
2026-04-26 11:43:13 -0400

    Update FinalProject_Kmeans_andPCS.py
    Added Classification Table to compare to the rest of the document

--- FinalProject_Kmeans_andPCS.py
+++ FinalProject_Kmeans_andPCS.py
@@ -5,6 +5,7 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.metrics import classification_report, precision_score, recall_score
 
 #Load and Prepare Data
 df= pd.read_csv('results.csv', encoding='latin1')
@@ -15,31 +16,38 @@ df_clean = df.dropna(subset=features).copy()
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(df_clean[features])
 
-#Perform Principal Component Analysis
 pca = PCA(n_components =3)
 X_pca_3d = pca.fit_transform(X_scaled)
 
-#Provides List of Most Important items for Each Component
 loadings = pd.DataFrame(pca.components_.T, columns=['PC1', 'PC2', 'PC3'], index=features)
 print(loadings.sort_values(by='PC1', key=abs, ascending=False).head(16))
 
-#Performs K-Means Clustering
 kmeans= KMeans(n_clusters=3, random_state=42, n_init=10)
 df_clean['Cluster'] = kmeans.fit_predict(X_pca_3d)
 
-#Labels each Cluster with how each type of performamce
 cluster_map = { 0: "Away Team Dominance", 1:"Home Team Dominance", 2: "Competitive"}
 df_clean['ClusterLabel'] = df_clean['Cluster'].map(cluster_map)
 
-#Provides Table of Average Perfomance for Each cluster
 summary_table = df_clean.groupby('ClusterLabel')[features].mean()
 summary_table_transposed = summary_table.T
 
 print("\n ---Cluster Summary Table (Feature Average) ---")
 print(summary_table_transposed)
 
+true_label_map = {'A': 'Away Team Dominance', 'H': 'Home Team Dominance', 'D':'Competitive'}
+df_clean['TrueLabel'] = df_clean['FTR'].map(true_label_map)
 
-#Visualization of the Cluster
+precision = precision_score(df_clean['TrueLabel'], df_clean['ClusterLabel'], average='weighted')
+recall = recall_score(df_clean['TrueLabel'], df_clean['ClusterLabel'], average='weighted')
+
+print("\n --- Model Metrics---")
+print(f"Weighted Precision: {precision:.4f}")
+print(f"Weighted Recall: {recall:.4f}")
+
+print("\n--- Classification Report---")
+print(classification_report(df_clean['TrueLabel'], df_clean['ClusterLabel']))
+
+#Visualization
 fig = plt.figure(figsize=(12,9))
 ax = fig.add_subplot(111, projection='3d')
 
